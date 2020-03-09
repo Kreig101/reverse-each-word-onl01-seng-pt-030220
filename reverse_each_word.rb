5ecad71855746dc0b7a80f227122f9b7b51a1253
Craig Storer <craigstorer@gmail.com>
2020-03-09 19:22:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,20 @@
 require 'pry'
-def reverse_each_word(sentence)
-  y = []
- 
-sentence.split.each do |words|
-   binding.pry
-  y << words.reverse
- 
-end
-y
-end
- 
 # def reverse_each_word(sentence)
+#   y = []
+ 
+# sentence.split.each do |words|
+ 
+#   y << words.reverse
+ 
+# end
+# y
+# end
+ 
+def reverse_each_word(sentence)
   
-#   sentence.split.collect do |words|
-#   puts  words.reverse
-
+  sentence.split.collect do |words|
+  puts  words.reverse
+    binding.pry
     
-# end
-# end
+end
+end
