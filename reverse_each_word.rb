fed6b8ccf85bb0411b34c40532e4e0f8eb5a6e03
Craig Storer <craigstorer@gmail.com>
2020-03-09 20:01:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,10 +11,10 @@ require 'pry'
  
 def reverse_each_word(sentence)
   
-  sentence.split.collect do {|words|
-   words.reverse
+  sentence.split.collect dosentence.split.collect{|word| word.reverse}.join(' ')
+  
 
     
-end.join(" ")
+end
 
 end
